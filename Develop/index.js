bb6cce0c930ae0a2e8922470b2444f27aa23e0f9
Jillium <86424914+Jillium@users.noreply.github.com>
2021-11-04 10:37:18 -0600

    prompts added

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+// const generatePage = require('.utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -32,9 +33,14 @@ const promptUser = () => {
                 }
             }
         },
+     ])
+}
+
+const promptProject = () => {
+    return inquirer.prompt([
         {
             type: 'input',
-            name: 'project Title',
+            name: 'project-Title',
             message: 'What is your project name? (Required)',
             validate: nameInput => {
                 if (nameInput) {
@@ -45,27 +51,80 @@ const promptUser = () => {
                 }
             }
 
+        },
+        {
+            type: 'input',
+            name: 'project-description',
+            message: 'Write something about your project. Such as description, motivation, or why you made it. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project-description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'project-languages',
+            message: 'What languages did you use to create this application? Select all that apply.',
+            choices: ['Javascript', 'CSS', 'HTML', 'Node', 'Other']
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What steps are needed to install the application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What will this application be used for?'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Who were contributors on this project?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select the appropriate license for this project',
+            choices: [
+                'Apache',
+                'GNU',
+                'GPL',
+                'ISC',
+                'MIT',
+                'Open'
+            ]
+        },
+        {
+            // badges
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Are there any tests included? Please describe.'
+        },
+        {
+            type: 'input',
+            name: 'contact-info',
+            message: 'Please enter your Github username'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email:'
         }
+
+
     ])
 }
 
 
 // TODO: Create a function to write README file
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('newReadMe.md', fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve({
-                ok: true,
-                message: 'File created!'
-            });
-        });
-    });
-};
+function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -74,6 +133,7 @@ function init() {}
 init();
 
 promptUser()
-    .then(writeFile => {
-        console.log('hello');
+    .then(promptProject)
+    .then(writeToFile => {
+        
     });
